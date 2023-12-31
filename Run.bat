c4c35f8469e19687fa9d5f7df1a4bf96c5dd71b8
tibisan <tibisan@users.noreply.github.com>
2023-12-31 22:27:19 +0200

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,4 +1,8 @@
+
+PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
+
 cls
+@ECHO Please wait while we uninstall OneDrive
 
 set x86="%SYSTEMROOT%\System32\OneDriveSetup.exe"
 set x64="%SYSTEMROOT%\SysWOW64\OneDriveSetup.exe"
@@ -29,4 +33,9 @@ echo.
 REG DELETE "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > NUL 2>&1
 REG DELETE "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > NUL 2>&1
 
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
+@ECHO Please wait while we install Brave Browser
+
+powershell -command "Invoke-WebRequest 'https://brave-browser-downloads.s3.brave.com/latest/brave_installer-x64.exe'  -OutFile $env:temp\brave_installer-x64.exe"
+powershell -command "Start-Process -FilePath  $env:temp\brave_installer-x64.exe -ArgumentList '--install --silent --system-level' -Wait"
+
+
